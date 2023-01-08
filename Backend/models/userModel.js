c18d7191b931656329265ae75b038dd137d5dcfd
Shridhar600 <65012105+Shridhar600@users.noreply.github.com>
2023-01-08 22:57:54 +0530

    registration backend done

---
+++ Backend/models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://www.flaticon.com/free-icon/profile-user_64572?term=user&page=1&position=13&origin=search&related_id=64572",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// to encrypt the password we use bcrypt;
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const notes = require("./data/notes");
+const userRoutes = require("./routes/userRoutes")
 const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middlewares/middleware");
 
 dotenv.config();
 const app = express();
-
 connectDB();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is ON.");
@@ -16,6 +18,10 @@ app.get("/api/notes", (req, res) => {
   res.send(notes);
 });
 
+app.use('/api/users', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 //req.params.id to fetch particular data from the data set.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port:${PORT}.`));
